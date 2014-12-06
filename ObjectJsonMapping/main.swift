932908add024a10667a219d18073ab0b1bbe5e26
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-06 17:34:28 +0900

    Initial Commit

---
+++ ObjectJsonMapping/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjectJsonMapping
+//
+//  Created by 森下 健 on 2014/12/06.
+//  Copyright (c) 2014年 Yumemi. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
