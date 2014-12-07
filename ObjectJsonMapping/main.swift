37bfa9911f91c0c49357a7b69b82752eca230701
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-07 22:57:12 +0900

    Arrayaで始まるJSONの検証を追加

--- ObjectJsonMapping/main.swift
+++ ObjectJsonMapping/main.swift
@@ -17,10 +17,10 @@ enum ResultType : String {
 func checkJsonEntity<ET : JsonGenEntityBase>(EntityType: ET.Type, filename: String) -> ResultType {
     var data = NSData(contentsOfFile: filename)!
     
-    if let b1 = ET.fromData(data) {
-      println(b1.toJsonString())
+    if let b1 = ET.fromData(data) as? ET {
+        println(b1.toJsonString(pritty: true))
         let d1 = b1.toJsonData()
-        let b2 = ET.fromData(d1)!
+        let b2 = ET.fromData(d1) as ET
         let d2 = b2.toJsonString()
         if b1.toJsonString() == b2.toJsonString() {
             return .OK
@@ -30,7 +30,22 @@ func checkJsonEntity<ET : JsonGenEntityBase>(EntityType: ET.Type, filename: Stri
     } else {
         return .ParseNG
     }
-    
+}
+
+func checkJsonArray<ET : JsonGenEntityBase>(EntityType: ET.Type, filename: String) -> ResultType {
+    if let a1 = ET.fromData(NSData(contentsOfFile: filename)!) as? [ET] {
+        println(ET.toJsonString(a1, pritty: true))
+        let d1 = ET.toJsonData(a1)
+        let a2 = ET.fromData(d1) as [ET]
+        let d2 = ET.toJsonData(a2)
+        if ET.toJsonString(a1) == ET.toJsonString(a2) {
+            return .OK
+        } else {
+            return .RepeatNG
+        }
+    } else {
+        return .ParseNG
+    }
 }
 
 let bundle = NSBundle.mainBundle()
@@ -42,6 +57,7 @@ ret["Order should be OK: "] = checkJsonEntity(Order.self, bundle.pathForResource
 ret["TypeCheck should be OK: "] = checkJsonEntity(TypeCheck.self, bundle.pathForResource("TypeCheck", ofType: "json")!)
 ret["bookError should be ParseNG: "] = checkJsonEntity(TypeCheck.self, bundle.pathForResource("bookError", ofType: "json")!)
 ret["bookErrorLackKey should be ParseNG: "] = checkJsonEntity(TypeCheck.self, bundle.pathForResource("bookErrorLackKey", ofType: "json")!)
+ret["[Author] should be OK: "] = checkJsonArray(Author.self, bundle.pathForResource("authors", ofType: "json")!)
 
 for (name, result) in ret {
     println("\(name) \(result.rawValue)")
